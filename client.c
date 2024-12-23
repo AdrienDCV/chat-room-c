c10d1a07b85bd1691b2701e1df99c4fa64cbe4e5
Adrien DA COSTA VEIGA <adrien.dacostaveiga@jetdev.fr>
2024-12-23 21:28:07 +0100

    ✨ : add logs file and start rework code

--- client.c
+++ client.c
@@ -18,18 +18,14 @@ int sock_fd = 0;
 int disconnection_flag = 0;
 char username[USERNAME_SIZE];
 
-void remove_carriage_return_char (char* msg, int length) {
-  int i;
-  for (i = 0; i < length; i++) {
-    if (msg[i] == '\n') {
-      msg[i] = '\0';
-      break;
+void remove_carriage_return_char (char* message) {
+    int message_length = strlen(message) - 1;
+    if (message[message_length] == '\n') {
+      message[message_length] = '\0';
     }
-  }
 }
 
 void send_message() {
-
     char message[MSG_SIZE];
     char buffer[BUFFER_SIZE];
     time_t current_time;
@@ -40,9 +36,9 @@ void send_message() {
     while (should_run) {
 
         fgets(message, MSG_SIZE, stdin);
-        printf("\033[1A\033[2K\r"); // Remonte d'une ligne et efface
+        printf("\033[1A\033[2K\r");
         fflush(stdout);
-        remove_carriage_return_char(message, MSG_SIZE);
+        remove_carriage_return_char(message);
     
         if (strcmp(message, "exit") == 0) {
             sprintf(buffer, "%s", message);
@@ -51,7 +47,7 @@ void send_message() {
         } else if (strlen(message) > 0) {
             time(&current_time);
             struct tm *local_time = localtime(&current_time);
-            strftime(message_prefix, sizeof(message_prefix), "(%d-%m/%H:%M) ", local_time);
+            strftime(message_prefix, sizeof(message_prefix), "(%d-%m-%Y/%H:%M) ", local_time);
             strcat(message_prefix, username);
 
             snprintf(buffer, sizeof(buffer), "%s : %s\n", message_prefix, message);
@@ -61,9 +57,7 @@ void send_message() {
 
         bzero(message, MSG_SIZE);
         bzero(buffer, MSG_SIZE);
-
     }
-
 }
 
 void receive_message() {
@@ -86,67 +80,65 @@ void receive_message() {
 
 }
 
-int main(int argc, char const* argv[]){
-    int status;
+int main(int argc, char const* argv[]) {
     struct sockaddr_in server_addr;
+    server_addr.sin_addr.s_addr = inet_addr(IP_SERVER);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
 
-    printf("Enter your username: ");
+
+    printf("CLIENT: Enter your username: ");
     if (fgets(username, sizeof(username), stdin) == NULL) {
-        fprintf(stderr, "Error reading input.\n");
-        return -1;
+        fprintf(stderr, "CLIENT: Error reading input.\n");
+        return EXIT_FAILURE;
     }
-
-    if (username[strlen(username) - 1] != '\n') {
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF);
-
-        fprintf(stderr, "Username is too long. Maximum length is %d characters.\n", (USERNAME_SIZE-1));
-        return -1;
+    if (strlen(username) > USERNAME_SIZE && username[strlen(username) - 1] != '\n') {
+        fprintf(stderr, "CLIENT-ERROR: Username is too long. Maximum length is %d characters.\n", (USERNAME_SIZE-1));
+        return EXIT_FAILURE;
     }
     username[strlen(username) - 1] = '\0';
 
 
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+        printf("CLIENT-ERROR: socket: could not open the socket\n");
+        return EXIT_FAILURE;
     }
-    server_addr.sin_addr.s_addr = inet_addr(IP_SERVER);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
 
     if (inet_pton(AF_INET, IP_SERVER, &server_addr.sin_addr)<= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        printf("ERROR: inet_pton: invalid address or address not supported\n");
+        return EXIT_FAILURE;
     }
 
-    status = connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if (status == -1) {
-        printf("\nConnection Failed \n");
-        return 1;
+    if (connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        printf("ERROR: connect: connection Failed\n");
+        return EXIT_FAILURE;
     }
 
-    send(sock_fd, username, 128, 0);
+    send(sock_fd, username, USERNAME_SIZE, 0);
 
     fflush(stdout);
+    printf("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n");
     printf("≈≈≈≈≈≈≈≈≈ FISA3 - CHAT ROOM ≈≈≈≈≈≈≈≈≈\n");
-    printf("You have joined the chat room\n");
+    printf("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n");
+    printf("CLIENT-INFO: You have joined the chat room\n");
 
     pthread_t send_msg_thread;
     if(pthread_create(&send_msg_thread, NULL, (void *) send_message, NULL) != 0){
-        printf("ERROR: pthread\n");
+        printf("CLIENT-ERROR: pthread: could not create thread\n");
         return EXIT_FAILURE;
     }
 
     pthread_t receive_msg_thread;
     if(pthread_create(&receive_msg_thread, NULL, (void *) receive_message, NULL) != 0){
-        printf("ERROR: pthread\n");
+        printf("CLIENT-ERROR: pthread: could not create thread\n");
         return EXIT_FAILURE;
     }
     
-    while (1) {
+    int should_run = 1;
+    while (should_run) {
         if (disconnection_flag) {
-            printf("Disconnected\n");
+            printf("CLIENT-INFO: You have been successfuly disconnected from the room\n");
             break;
         }
     }
--- server.c
+++ server.c
@@ -12,6 +12,7 @@
 #define BUFFER_SIZE 2048
 #define CHAT_ROOM_SIZE 5
 #define USERNAME_LENGTH 255
+#define LOGS_FILE_NAME_SIZE 128
     
 typedef struct
 {
@@ -24,43 +25,89 @@ int uid = 1;
 int nb_users;
 User *users[CHAT_ROOM_SIZE];
 
+char logs_file_name[64] = "chat-room-logs";
+FILE *file_pointer;
+
 pthread_mutex_t users_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t server_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void init_socket(int *server_fd) {
     if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("ERROR: socket: could not open the socket");
+        perror("LOG-ERROR : init_socket: could not open the socket\n");
         exit(EXIT_FAILURE);
     }
-    printf("LOG-INFO : Socket opened\n");
+    printf("LOG-INFO : init_socket : Socket opened\n");
 }
 
-void remove_carriage_return_char (char* msg, int length) {
-  int i;
-  for (i = 0; i < length; i++) {
-    if (msg[i] == '\n') {
-      msg[i] = '\0';
-      break;
+int open_logs_file() {
+    if (file_pointer != NULL) {
+        return 0;
     }
-  }
+
+    char log_file_name[LOGS_FILE_NAME_SIZE];
+    char log_file_time[64];
+
+    time_t current_time;
+    time(&current_time);
+    struct tm *local_time = localtime(&current_time);
+    strftime(log_file_time, sizeof(log_file_time), "-%Y-%m-%d", local_time);
+    snprintf(log_file_name, sizeof(log_file_name), "chat-room-log%s.txt", log_file_time);
+
+    file_pointer = fopen(log_file_name, "a");
+
+    if (file_pointer == NULL) {
+        perror("SERVER-ERROR: open_logs: Could not open logs file.");
+        return 1;
+    }
+
+    return 0;
 }
 
-void send_message(char *s){
-	pthread_mutex_lock(&users_mutex);
+int close_logs_file() {
+    if (file_pointer != NULL) {
+        if (fclose(file_pointer) == 0) {
+            file_pointer = NULL;
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void write_logs(char *line) {
+    pthread_mutex_lock(&server_mutex);
+
+
+    if (open_logs_file() == 0) {
+        line[strlen(line) - 1] = '\0';
+        fprintf(file_pointer, "%s\n", line);
+        close_logs_file();
+    } else {
+        printf("SERVER-ERROR: write_logs: Could not right in logs file.");
+    }
+
+    pthread_mutex_unlock(&server_mutex);
+}
+
+void broadcast_message(char *message){
+	pthread_mutex_lock(&server_mutex);
 
 	for(int i=0; i<CHAT_ROOM_SIZE; ++i){
 		if(users[i]){
-			if(write(users[i]->sockfd, s, strlen(s)) < 0){
-				perror("ERROR: write to descriptor failed");
+			if(write(users[i]->sockfd, message, strlen(message)) < 0){
+				perror("LOG-ERROR : send_message : could not send message\n");
 				break;
 			}
 		}
 	}
 
-	pthread_mutex_unlock(&users_mutex);
+	pthread_mutex_unlock(&server_mutex);
+
 }
 
 void add_user_in_room(User* user) {
-	pthread_mutex_lock(&users_mutex);
+    pthread_mutex_lock(&users_mutex);
 
 	for(int i = 0; i < CHAT_ROOM_SIZE; i++){
 		if(!users[i]) {
@@ -69,80 +116,80 @@ void add_user_in_room(User* user) {
 		}
 	}
 
-	pthread_mutex_unlock(&users_mutex);
+    pthread_mutex_unlock(&users_mutex);
 }
 
-void remove_user_froom_room(int uid) {
-	pthread_mutex_lock(&users_mutex);
+void remove_user_from_list(int uid) {
+    pthread_mutex_lock(&users_mutex);
 
 	for(int i = 0; i < CHAT_ROOM_SIZE; i++){
         if (users[i] && users[i]->uid == uid) {
             users[i] = NULL;
-            printf("LOG-INFO : user removed\n");
+            printf("LOG-INFO : remove_user_from_room : user removed\n");
             break;
         }
 	}
+    pthread_mutex_unlock(&users_mutex);
+}
 
-	pthread_mutex_unlock(&users_mutex);
+void remove_user_from_room(User *user) {
+    close(user->sockfd);
+    remove_user_from_list(user->uid);
+  	free(user);
+  	nb_users--;
 }
 
 User* create_user(int client_sock_fd) {
     User *user = (User *)malloc(sizeof(user));
     user->sockfd = client_sock_fd;
     user->uid = uid++;
+    nb_users++;
 
     return user;
 }
 
-void *listen_to_client(void* arg) {
+void listen_username(User *user, char *username, char *buffer) {
+    if (recv(user->sockfd, username, USERNAME_LENGTH, 0) > 0) {
+        strcpy(user->username, username);
+        sprintf(buffer, "SERVER-INFO : %s has joined the chat\n", user->username);
+        printf("%s", buffer);
+        broadcast_message(buffer);
+        write_logs(buffer);
+    }
+}
 
+void *listen_to_client(void* arg) {
     char buffer[BUFFER_SIZE];
     char username[USERNAME_LENGTH];
     time_t current_time;
-    
-    nb_users++;
-    User *user = (User *)arg;
+    int *client_socket_fd = (int *)arg;
 
-    if (recv(user->sockfd, username, USERNAME_LENGTH, 0) > 0) {
-        strcpy(user->username, username);
-        sprintf(buffer, "%s has joined the chat\n", user->username);
-        printf("%s", buffer);
-        send_message(buffer);
-    }
+    User* user = create_user(*client_socket_fd);
+    add_user_in_room(user);
 
+    listen_username(user, username, buffer);
     bzero(buffer, BUFFER_SIZE);
 
-    int should_stop = 0;
     int should_run = 1;
     while (should_run) {
-
-        if (should_stop) {
-            break;
-        }
         
         int receive = recv(user->sockfd, buffer, BUFFER_SIZE, 0);
         if (receive > 0){
             if (strcmp(buffer, "exit") == 0) {
-                sprintf(buffer, "%s has left the chat\n", user->username);
-                send_message(buffer);
-                should_stop = 1;
+                sprintf(buffer, "SERVER-INFO : %s has left the chat\n", user->username);
+                broadcast_message(buffer);
+                should_run = 0;
             } else if (strlen(buffer) > 0){
-				send_message(buffer);
-
-				remove_carriage_return_char(buffer, strlen(buffer));
-				printf("%s\n", buffer);
+				broadcast_message(buffer);
+				printf("%s", buffer);
 			}
 
+            write_logs(buffer);
             bzero(buffer, BUFFER_SIZE);
-
 		}
-
     }
 
-    close(user->sockfd);
-    remove_user_froom_room(user->uid);
-  	free(user);
-  	nb_users--;
+    remove_user_from_room(user);
  	pthread_detach(pthread_self());
 
     return NULL;
@@ -157,27 +204,35 @@ int main(int argc, char const* argv[]) {
     int opt = 1;
     pthread_t listen_thread, send_thread;
 
-    init_socket(&socket_fd);
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
+    init_socket(&socket_fd);
+
     if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        perror("ERROR: setsockopt: could not set socket options properly");
+        perror("LOG-ERROR: setsockopt: could not set socket options properly");
         return EXIT_FAILURE;
     }
     if (bind(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("ERROR: bind: could not properly bind the socket to IP address");
+        perror("LOG-ERROR: bind: could not properly bind the socket to IP address");
         return EXIT_FAILURE;
     }
 
     if (listen(socket_fd, CHAT_ROOM_SIZE) < 0) {
-        perror("ERROR: Socket listening failed");
+        perror("LOG-ERROR: listen: Socket listening failed");
         return EXIT_FAILURE;
 	}
 
+    if (open_logs_file() == EXIT_FAILURE) {
+        printf("SERVER-ERROR: open_logs_file: Could not open logs file.");
+        return EXIT_FAILURE;
+    }
+
+
+    printf("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n");
     printf("≈≈≈≈≈≈≈≈≈ FISA3 - CHAT ROOM ≈≈≈≈≈≈≈≈≈\n");
+    printf("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n");
     int should_run = 1;
     while (should_run) {
 
@@ -185,15 +240,12 @@ int main(int argc, char const* argv[]) {
         client_sock_fd = accept(socket_fd, (struct sockaddr*)&client_addr, &client_addr_len);
 
         if ((nb_users + 1) == CHAT_ROOM_SIZE) {
-            printf("The chat room is full. Connection denied.");
+            printf("SERVER-INFO : The chat room is full. Connection denied.");
             close(client_sock_fd);
             continue;
         }
 
-        User* user = create_user(client_sock_fd);
-        add_user_in_room(user);
-
-        pthread_create(&listen_thread, NULL, &listen_to_client, (void*)user);
+        pthread_create(&listen_thread, NULL, &listen_to_client, (void*)&client_sock_fd);
 
         sleep(1);
 
